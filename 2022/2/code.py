fdbe4325581342930d2e7ec1fd5e1e531ef5c4b6
Robin Taylor <otayloro@amazon.com>
2023-11-23 10:07:19 +0000

    part 2

--- 2022/2/code.py
+++ 2022/2/code.py
@@ -10,6 +10,7 @@ with open((__file__.rstrip("code.py") + "input.txt"), "r") as input_file:
 class Solver:
     def __init__(self, input) -> None:
         self.my_score: int = 0
+        self.b_score: int = 0
         self.scoring: dict = {"A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3}
         a: list[str] = input.split("\n")
         self.hands: dict[str, str] = []
@@ -34,16 +35,47 @@ class Solver:
             return
         return
 
+    def _evaluate_hand_strat_b(self, theirs: int, mine: int) -> None:
+        # Win
+        if mine == "Z":
+            self.b_score += 6
+            if theirs == "A":
+                self.b_score += 2
+            if theirs == "B":
+                self.b_score += 3
+            if theirs == "C":
+                self.b_score += 1
+            return
+
+        # Draw
+        if mine == "Y":
+            self.b_score += 3
+            self.b_score += self.scoring[theirs]
+            return
+
+        # Lose
+        if mine == "X":
+            if theirs == "A":
+                self.b_score += 3
+            if theirs == "B":
+                self.b_score += 1
+            if theirs == "C":
+                self.b_score += 2
+            return
+        return
+
     def play(self):
         score = 0
         for hand in self.hands:
             their_play = hand[0]
             my_play = hand[1]
             self._evaluate_hand(theirs=their_play, mine=my_play)
+            self._evaluate_hand_strat_b(theirs=their_play, mine=my_play)
 
     def run(self):
         self.play()
         print(self.my_score)
+        print(self.b_score)
 
 
 if __name__ == "__main__":
