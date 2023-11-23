561f2452811a3abfa9dad40f0646d7df8d65ff3a
Robin Taylor <otayloro@amazon.com>
2023-11-23 11:52:54 +0000

    day 1 part 2 that I missed

--- 2022/1/code.py
+++ 2022/1/code.py
@@ -8,6 +8,9 @@ with open((__file__.rstrip("code.py") + "input.txt"), "r") as input_file:
 class Solver:
     def __init__(self, input) -> None:
         self.input = input
+        self.first: int = 0
+        self.second: int = 0
+        self.third: int = 0
         pass
 
     def separate_input(self) -> list[int]:
@@ -15,25 +18,28 @@ class Solver:
         result = [list(map(int, elf.split("\n"))) for elf in a]
         return result
 
-    def biggest_elf(self, elves: list[int]) -> int:
+    def process_total(self, inspection: int):
+        if inspection > self.first:
+            self.first, inspection = inspection, self.first
+        if inspection > self.second:
+            self.second, inspection = inspection, self.second
+        if inspection > self.third:
+            self.third, inspection = inspection, self.third
+        return
+
+    def biggest_elf(self, elves: list[int]) -> None:
         current_total = 0
         for elf in elves:
-            print(elf)
             total = sum(elf)
-            if total > current_total:
-                current_total = total
+            self.process_total(inspection=total)
         return current_total
 
     def run(self):
         elves = self.separate_input()
-        highest_elf = self.biggest_elf(elves)
-        print(f"biggest - {highest_elf}")
+        self.biggest_elf(elves)
+        print(f"biggest - {self.first}")
+        print(f"combined = {sum([self.first,self.second, self.third])}")
 
 
 s = Solver(input=input)
 s.run()
-
-print("Part One : " + str(None))
-
-
-print("Part Two : " + str(None))
