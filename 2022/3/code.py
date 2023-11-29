1854e530bebfb06eff2165cff55b6c1c80e3a37f
Robin Taylor <otayloro@amazon.com>
2023-11-29 14:07:17 +0000

    Day 3

--- 2022/3/code.py
+++ 2022/3/code.py
@@ -5,7 +5,52 @@ with open((__file__.rstrip("code.py") + "input.txt"), "r") as input_file:
     input = input_file.read()
 
 
-print("Part One : " + str(None))
+class Solver:
+    def __init__(self, input) -> None:
+        self.input = input
+
+    def process_items(self, items: list[str]) -> int:
+        score = 0
+        for item in items:
+            score += ord(item.lower()) - 96
+            if item.isupper():
+                score += 26
+        return score
+
+    def process_pockets(self, first_pocket, second_pocket) -> str:
+        return "".join(set(first_pocket).intersection(second_pocket))
+
+    def puzzle_one(self):
+        items = []
+        for rucksack in self.input.split("\n"):
+            first_pocket = rucksack[: len(rucksack) // 2]
+            second_pocket = rucksack[len(rucksack) // 2 :]
+            items.append(
+                self.process_pockets(
+                    first_pocket=first_pocket, second_pocket=second_pocket
+                )
+            )
+
+        score = self.process_items(items=items)
+        print(f"score 1 is {score}")
+
+    def puzzle_two(self):
+        rucksacks = self.input.split("\n")
+        score = []
+        while len(rucksacks) > 0:
+            first = set(rucksacks.pop())
+            second = set(rucksacks.pop())
+            third = set(rucksacks.pop())
+            overlap = first.intersection(second).intersection(third).pop()
+            score.append(overlap)
+        score = self.process_items(items=score)
+        print(f"score 2 is {score}")
+
+    def run(self):
+        self.puzzle_one()
+        self.puzzle_two()
 
 
-print("Part Two : " + str(None))
+if __name__ == "__main__":
+    s = Solver(input=input)
+    s.run()
